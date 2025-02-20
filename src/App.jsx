8a006eb453c3f8606c539720ab176a9049064bdb
Rakibul-Islam-GitHub <rakibulislam.cse21@gmail.com>
2025-02-21 03:19:03 +0600

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
-function App() {
-  return <></>;
-}
+import Header from "./components/Header";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+import { Provider } from "react-redux";
+import { store } from "./store"; // Import the store
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 );
